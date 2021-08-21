3eecffd350eace3e8a16d647ca05427ccff56c63
Dipak Bari <dipakbari4@gmail.com>
2021-08-22 00:14:37 +0530

    Animal functionality added

---
+++ src/com/dipak/j2se/Animal.java
@@ -0,0 +1,9 @@
+package com.dipak.j2se;
+
+public abstract class Animal {
+	public abstract void animalSound();
+
+	public void sleep() {
+		System.out.println("Zzzz...");
+	}
+}
---
+++ src/com/dipak/j2se/Cat.java
@@ -0,0 +1,12 @@
+package com.dipak.j2se;
+
+public class Cat extends Animal{
+
+	@Override
+	public void animalSound() {
+		// TODO Auto-generated method stub
+		System.out.println("Meow meow....");
+		
+	}
+	
+}
